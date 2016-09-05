f1e5ffc1777b9f1047a1ec7758964bd2466dcb12
Darryn Patterson <darryn@whereismytransport.com>
2016-09-05 07:04:54 +0200

    Project initialization.

---
+++ src/AspNetCoreNoCache/NoCacheMiddleware.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace AspNetCoreNoCache
+{
+    public class NoCacheMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public NoCacheMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            context.Response.Headers.Add("Cache-Control", "no-cache, no-store, must-revalidate");
+            context.Response.Headers.Add("Pragma", "no-cache");
+            context.Response.Headers.Add("Expires", "0");
+
+            await _next(context);
+        }
+    }
+}
